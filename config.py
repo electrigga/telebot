a1f14713f232da3936f52782d591f3454613f9b4
Jens Bremeier <electrigga@gmx.net>
2018-03-17 19:36:30 +0100

    Erstes Update

--- config.py
+++ config.py
@@ -3,8 +3,11 @@
 # Hier den API Schlüssel eintragen, den man vom Botfather erhalten hat
 apikey = ""
 
-# ACL für die Steuerung
+# ACL für die Steuerung [id1,id2,....]
 grant = []
 
 # ID des Botowner / der Botowner (die bekommen Nachrichten über Statusänderungen)
 owner = []
+
+# Rufzeichen/Name des Repeaters, auf dem der Bot läuft
+botcall = ""
--- telebot.py
+++ telebot.py
@@ -1,35 +1,56 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-import telepot, sys, time, datetime, RPi.GPIO as GPIO
+import telepot, sys, time, datetime, psutil, RPi.GPIO as GPIO
 
 # Variablen aus der Config holen
 from config import apikey
 from config import grant
 from config import owner
+from config import botcall
 
 # GPIO Settings
+GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(13, GPIO.OUT)
 GPIO.setup(15, GPIO.OUT)
 
+# Funktion zur Information des/der Botowner
 def ownerinfo(msg,owner):
     for x in owner:
         bot.sendMessage(x,msg)
 
 def handle(msg):
-    chat_id = msg['chat']['id']
+    content_type, chat_type, chat_id = telepot.glance(msg)
+    #print(content_type, chat_type, chat_id)
+
     command = msg['text']
     vorname = msg['from']['first_name']
     username = msg['from']['username']
     id = msg['from']['id']
 
-    # print(msg)
+    print(msg)
+    # print 'Kommando erhalten: %s von %s' % (command, username)
 
-    print 'Kommando erhalten: %s von %s' % (command, username)
+    if msg['text'] in ["/start","/start start", "start", "hallo", "Hallo", "Hi", "Start"]:
+	bot.sendMessage(chat_id, "Herzlich willkommen bei " + botcall + " " + vorname + "!" + \
+				 "\nUm Hilfe zu erhalten, schreib /hilfe. Informationen und Hinweise bitte an @dl2ajb.")
 
-    if command == '/start':
-	bot.sendMessage(chat_id, "Herzlich willkommen bei DB0ASE " + vorname + "!")
-    elif command == '/status':
+    elif msg['text'] in ["/proc"]:
+	for proc in psutil.process_iter():
+	    try:
+		pinfo = proc.as_dict(attrs=['pid', 'name', 'username'])
+	    except psutil.NoSuchProcess:
+                pass
+	    else:
+		bot.sendMessage(chat_id,pinfo)
+
+    elif msg['text'] in ["/hilfe"]:
+	hilfetext = "Informationen und Kommandos:\n/status Gibt den Status des Repeaters aus\n/hilfe Hilfetext mit der Liste der Kommandos"
+        if id in grant:
+            hilfetext += "\n/txan Schaltet den Sender an\n/txaus Schaltet den Sender aus\n/rxan Schaltet den RX ein\n/rxaus Schaltet den RX an"
+        bot.sendMessage(chat_id,botcall + " " + hilfetext)
+
+    elif msg['text'] in ["/status"]:
 	# Eingänge lesen
         if GPIO.input(13) == GPIO.HIGH:
 	    gpio13 = "aus"
@@ -42,26 +63,37 @@ def handle(msg):
 
         bot.sendMessage(chat_id, "TX ist " + gpio13 + "\n" + "RX ist " + gpio15)
 
-    elif command == '/txaus':
+    elif msg['text'] in ["/txaus"]:
         if id in grant:
             GPIO.output(13, GPIO.HIGH)
 	    bot.sendMessage(chat_id,"Sender ist aus!")
         else:
 	    bot.sendMessage(chat_id,"Das darfst du nicht!")
-    elif command == '/txan':
+    elif msg['text'] in ["/txan"]:
         if id in grant:
             GPIO.output(13, GPIO.LOW)
             bot.sendMessage(chat_id,"Sender ist wieder an!")
         else:
             bot.sendMessage(chat_id,"Das darfst du nicht!")
 
-
+    elif msg['text'] in ["/rxaus"]:
+        if id in grant:
+            GPIO.output(15, GPIO.HIGH)
+            bot.sendMessage(chat_id,"Empfang ist aus!")
+        else:
+            bot.sendMessage(chat_id,"Das darfst du nicht!")
+    elif msg['text'] in ["/rxan"]:
+        if id in grant:
+            GPIO.output(15, GPIO.LOW)
+            bot.sendMessage(chat_id,"Empfang ist wieder an!")
+        else:
+            bot.sendMessage(chat_id,"Das darfst du nicht!")
     else:
-	bot.sendMessage(chat_id, "Das kenne ich nicht, " + vorname + "!")
+	bot.sendMessage(chat_id, "Damit kann ich nichts anfangen, " + vorname + "!")
 
 bot = telepot.Bot(apikey)
 try:
-    ownerinfo("Ich bin wieder da",owner)
+    ownerinfo("Ich bin wieder da...",owner)
     bot.message_loop(handle)
 except:
     print("Irgendwas stimmt mit dem Bot nicht....")
@@ -72,4 +104,5 @@ try:
 
 except:
     print("Tschüss....")
-    GPIO.cleanup()
+    ownerinfo("Der Bot wird beendet...",owner)
+    # GPIO.cleanup()
