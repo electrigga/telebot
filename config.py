49587285bcc14e08f4b53c963ddbe417b93e0d74
Jens Bremeier [DL2AJB] <electrigga@gmx.net>
2018-03-28 19:32:35 +0000

    New configfile

--- config.py
+++ config.py
@@ -1,29 +1,56 @@
 # -*- coding: utf-8 -*-
 
-# Hier den API Schlüssel eintragen, den man vom Botfather erhalten hat
+# api_key you got from @botfather for your bot
 apikey = ""
 
-# ACL für die Steuerung User ID here
-grant = [,]
+# ACL for controll the repeater (your own ID should in here) (ID is NOT @username)
+grant = [123456789]
 
 # ID des Botowner / der Botowner (die bekommen Nachrichten über Statusänderungen)
-owner = []
+owner = [123456789]
 
-# Rufzeichen/Name des Repeaters, auf dem der Bot läuft
-botcall = ""
+# Call of repeater
+botcall = "BOTCALL"
 
 # Liste mit zu prüfenden Prozessen
-prozesse=["","",""]
+prozesse=["MMDVMHost","DMRGateway","YSFGateway","ircddbgatewayd"]
 
 # ID des Repeaters
-dmrid = ""
+dmrid = "47110815"
 
 # folder which contains mmdvm-logs
 mmdvmlogs = "/var/log/mmdvm"
+mmprefix = "MMDVM"
 
-# define pathes to 1-wire sensor data 
+# folder wich contains the dmr-gw logfiles
+gwlogs = "/var/log/mmdvm"
+gwprefix = "DMRGateway"
+
+# how to start mmdvm
+mmdvmaufruf = "/usr/bin/screen /home/pi/MMDVMHost/MMDVMHost /home/pi/MMDVMHost/MMDVM-DB0SBN.ini"
+# how to start dmrgw should it be active?
+dmrgwaufruf = "/usr/bin/screen /home/pi/DMRGateway/DMRGateway /home/pi/DMRGateway/DMRGateway.ini"
+dmrgwaktiv = 1
+# how to start ysfgw
+ysfgw = "sudo /etc/init.d/YSFGateway.sh start"
+ysfgwaktiv = 1
+# how to start ircdbbgw
+ircdbbgw = "sudo /etc/init.d/ircddbgateway start"
+ircdbbgwaktiv = 1
+
+logfile = "botlog.txt"
+userfile = "users.csv"
+
+# define pathes to 1-wire sensor data
 sensors = [
-  ["/sys/bus/w1/devices/28-03b429126461/w1_slave","Sender"],
-  ["/sys/bus/w1/devices/28-559d29126461/w1_slave","Empfänger"],
-  ["/sys/bus/w1/devices/28-a4ad29126461/w1_slave","Netzteil"]
+  ["/sys/bus/w1/devices/28-24d329126461/w1_slave","TX Temp"],
+  ["/sys/bus/w1/devices/28-a78d29126461/w1_slave","NT Temp"]
+]
+
+# define gpio ports
+gpioports = [
+  # [gpio number,"name",invers]
+  [11,"TX",0],
+  [12,"RX",0],
+  [13,"NS1",0],
 ]
