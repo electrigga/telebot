7e4757145ff6f7e53f8d039456fcd4291d13ea22
Jens <dj1jay@xray37.de>
2018-03-30 19:38:02 +0200

    Update svxlink.py

--- svxlink.py
+++ svxlink.py
@@ -1,10 +1,35 @@
-from config import SVXOn,SVXOff
+## Set path to Svxlink symlink > svxlink.conf "DTMF_CTRL_PTY=/path/to/pty" Repeater/SvxlinkLogic
 
 rep_logic = "/home/pi/remote/svx_pty.RepeaterLogic"
 
+# path to svxlink.log (LastHeard Echolink)
+
+svx_log = "/var/log/svxlink.log"
+
+# Set DTMF Commands ['ButtonName','DTMF-Command']
+# Repeater need Open Command DTMF * or other
+
 svxcommands = [
-		['link341','*950# 341#'],
-		['link951','*340# 951'],
-		['SvxSyopOff','*OFFCMD#'],
-		['SvxSysopOn','*OnlineCMD#']
-	      ]
+                ['link Thr','*950# 341#'],
+                ['link ATA','*340# 951#'],
+#               ['SvxSyopOff','*986170#'],
+#               ['SvxSysopOn','986171#'],
+                ['Ansage Info ','**#'],
+                ['SVXOn','986171#'],
+                ['SVXOff','*986170#'],
+                ['Trennen','##']
+              ]
+
+# SvxLink start & Stop Command
+
+SVXOff =  'sudo systemctl stop svxlink.service'
+SVXOn =  'sudo systemctl start svxlink.service'
+
+### Lastheard in ReflectorLogic
+
+svxlh = [
+          ['THR_Logic'],
+          ['ATA_Logic']
+        ]
+
+
