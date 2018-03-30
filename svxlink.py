503c7d80ba898c91da40eaef61bda3cf5db90a62
Jens Bremeier [DL2AJB] <electrigga@gmx.net>
2018-03-30 19:29:37 +0000

    SVX-Function included

--- svxlink.py
+++ svxlink.py
@@ -1,10 +1,19 @@
-from config import SVXOn,SVXOff
-
 rep_logic = "/home/pi/remote/svx_pty.RepeaterLogic"
+svx_log = "/var/log/svxlink.log"
 
+SVXOff =  'sudo systemctl stop svxlink.service'
+SVXOn =  'sudo systemctl start svxlink.service'
+
+### DTMF Commands
 svxcommands = [
 		['link341','*950# 341#'],
 		['link951','*340# 951'],
 		['SvxSyopOff','*OFFCMD#'],
 		['SvxSysopOn','*OnlineCMD#']
 	      ]
+
+### Lastheard for locig
+svxlh = [
+	  ['THR_Logic'],
+	  ['ATA_Logic']
+	]
